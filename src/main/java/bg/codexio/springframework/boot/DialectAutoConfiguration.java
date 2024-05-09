94b6fa577e01687f53bbc4d38df28d912f5998f7
Ivan Yonkov <ivan.yonkov@codexio.bg>
2024-05-09 13:29:09 +0300

    Deduct custom dialect from driver class name

--- src/main/java/bg/codexio/springframework/boot/DialectAutoConfiguration.java
+++ src/main/java/bg/codexio/springframework/boot/DialectAutoConfiguration.java
@@ -1,36 +1,52 @@
 package bg.codexio.springframework.boot;
 
+import bg.codexio.springframework.data.jpa.requery.resolver.function.ExtendedMysqlDialect;
+import bg.codexio.springframework.data.jpa.requery.resolver.function.ExtendedPostgresqlDialect;
+import org.hibernate.dialect.Dialect;
+import org.hibernate.dialect.OracleDialect;
+import org.hibernate.dialect.SQLServerDialect;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Objects;
+import java.util.function.Function;
+
 @Configuration
 @AutoConfigureBefore(HibernateJpaAutoConfiguration.class)
 public class DialectAutoConfiguration {
     @Bean
-    @ConditionalOnProperty(name = "app.use-mysql", havingValue = "true")
-    public HibernatePropertiesCustomizer mysqlHibernatePropertiesCustomizer() {
-        return properties -> properties.put(
-                "hibernate.dialect",
-                "bg.codexio.springframework.data.jpa.requery.resolver"
-                        + ".function.ExtendedMysqlDialect"
-        );
+    @ConditionalOnMissingBean
+    public Function<String, Class<? extends Dialect>> dialectDeterminer() {
+        return driverName -> switch (driverName) {
+            case "com.microsoft.sqlserver.jdbc.SQLServerDriver" -> SQLServerDialect.class;
+            case "oracle.jdbc.driver.OracleDriver" -> OracleDialect.class;
+            default -> null;
+        };
     }
 
     @Bean
-    @ConditionalOnProperty(
-            name = "app.use-mysql",
-            havingValue = "false",
-            matchIfMissing = true
-    )
-    public HibernatePropertiesCustomizer postgresqlHibernatePropertiesCustomizer() {
+    public HibernatePropertiesCustomizer customizeDialect(
+            DataSourceProperties dataSourceProperties,
+            Function<String, Class<? extends Dialect>> dialectDeterminer
+    ) {
+        var driverName = dataSourceProperties.determineDriverClassName();
         return properties -> properties.put(
                 "hibernate.dialect",
-                "bg.codexio.springframework.data.jpa.requery.resolver"
-                        + ".function.ExtendedPostgresqlDialect"
+                Objects
+                        .requireNonNullElse(
+                                dialectDeterminer.apply(driverName),
+                                switch (driverName) {
+                                    case "org.postgresql.Driver" -> ExtendedPostgresqlDialect.class;
+                                    case "com.mysql.cj.jdbc.Driver" -> ExtendedMysqlDialect.class;
+                                    default -> Dialect.class;
+                                }
+                        )
+                        .getName()
         );
     }
 }
