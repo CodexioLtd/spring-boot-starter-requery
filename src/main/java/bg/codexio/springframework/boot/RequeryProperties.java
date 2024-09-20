6acfb0f98c565ab0d9461c794113a4a4050d7c5e
Vassil Kamburov <vassil.kamburov@codexio.bg>
2024-09-20 15:48:25 +0300

    - added logging stating which adapters have been started

--- src/main/java/bg/codexio/springframework/boot/RequeryProperties.java
+++ src/main/java/bg/codexio/springframework/boot/RequeryProperties.java
@@ -1,6 +1,8 @@
 package bg.codexio.springframework.boot;
 
 import bg.codexio.springframework.data.jpa.requery.adapter.HttpFilterAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -8,6 +10,8 @@ import java.util.stream.Collectors;
 
 @Component
 public class RequeryProperties {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private final List<HttpFilterAdapter> activeAdapters;
 
     public RequeryProperties(
@@ -23,6 +27,13 @@ public class RequeryProperties {
                                                                               .contains(adapter.getClass()
                                                                                                .getSimpleName()))
                                                    .collect(Collectors.toList());
+        }
+        for (var activeAdapter : this.activeAdapters) {
+            this.logger.info(
+                    "{} has been registered as an active adapter",
+                    activeAdapter.getClass()
+                                 .getSimpleName()
+            );
         }
     }
 
